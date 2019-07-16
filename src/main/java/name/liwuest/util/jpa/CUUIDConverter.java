8c060799dcd49a1f7891371a51e950b11452dd98
BjoernWuest <BjoernWuest@NB-ELLI-0020>
2019-07-16 23:01:21 +0200

    Bugfix in JPA converter

--- src/main/java/name/liwuest/util/jpa/CUUIDConverter.java
+++ src/main/java/name/liwuest/util/jpa/CUUIDConverter.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
-@Converter(autoApply = true) public class CUUIDConverter implements AttributeConverter<UUID, String> {
-	@Override public String convertToDatabaseColumn(final UUID entityValue) { return Optional.ofNullable(entityValue).map(entityUuid -> entityUuid.toString()).orElse(null); }
-	@Override public UUID convertToEntityAttribute(final String databaseValue) { return Optional.ofNullable(databaseValue).map(databaseUuid -> UUID.fromString(databaseUuid)).orElse(null); }
+@Converter(autoApply = true) public class CUUIDConverter implements AttributeConverter<UUID, Object> {
+	@Override public Object convertToDatabaseColumn(final UUID entityValue) { return Optional.ofNullable(entityValue).map(entityUuid -> entityUuid).orElse(null); }
+	@Override public UUID convertToEntityAttribute(final Object databaseValue) { return Optional.ofNullable(databaseValue).map(databaseUuid -> UUID.class.cast(databaseUuid)).orElse(null); }
 }
